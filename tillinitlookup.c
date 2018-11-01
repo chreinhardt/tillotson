6ad9db840571e3b0ce0a3528e2df1a250b2610b8
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2018-11-01 14:02:28 +0100

    Fixed a typo.

--- tillinitlookup.c
+++ tillinitlookup.c
@@ -507,7 +507,7 @@ int tillIsBelowColdCurve(TILLMATERIAL *material,double rho,double u)
     if ((rho < material->rhomin) || (rho >= material->rhomax))
     {
         fprintf(stderr, "tillIsBelowColdCurve: rho= %g outside of the lookup table (iMat= %i, rhomin= %g, rhomax= %g).\n",
-                rho, material->iMat, material->rhomin, material->rhomax);
+                rho, material->iMaterial, material->rhomin, material->rhomax);
         assert(0);
     }
 
