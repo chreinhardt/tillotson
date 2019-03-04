4f754813ff79f3aae04700e4c523b511ef5c09e7
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2019-03-04 10:10:31 +0100

    Removed unused variables.

--- tillcalcpressure.c
+++ tillcalcpressure.c
@@ -3,6 +3,7 @@
  *
  * Author:   Christian Reinhardt
  * Date:     01.10.2018
+ * Modified: 04.03.2019
  */
 #include <math.h>
 #include <stdio.h>
@@ -10,27 +11,15 @@
 #include <assert.h>
 #include "tillotson.h"
 
-#define max(A,B) ((A) > (B) ? (A) : (B))
-#define min(A,B) ((A) > (B) ? (B) : (A))
-
 #define INDEX(i, j) ((i*tillmat->nTableV) + (j))
 
-void main(int argc, char **argv) {
+int main(int argc, char **argv) {
+    // Tillotson EOS library
+    TILLMATERIAL *tillmat;
 	double dKpcUnit = 2.06701e-13;
 	double dMsolUnit = 4.80438e-08;
-	// These values are arbitrary
-	double rhomax = 100.0;
-	double vmax = 1200.0;
 	double rho, u, P;
-	int nTableRho = 1000;
-	int nTableV = 1000;
 	int iMat = 0;
-
-	int i = 0;
-	int j = 0;
-
-	TILLMATERIAL *tillmat;
-	struct lookup *isentrope;
 
 	if (argc != 4)
 	{
@@ -50,7 +39,9 @@ void main(int argc, char **argv) {
 	tillmat = tillInitMaterial(iMat, dKpcUnit, dMsolUnit);
 	
 	P = tillPressure(tillmat, rho, u);
-	printf("%i %.8g %.8g %.8g\n",iMat,rho,u,P);
+	printf("%i %.8g %.8g %.8g\n", iMat, rho, u, P);
 
 	tillFinalizeMaterial(tillmat);
+
+    return 0;
 }
