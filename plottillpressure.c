8488b2682d9629513246e3e0770696a047859d8e
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2019-03-09 08:11:39 +0100

    Doing a pressure plot in rho x u and rho x T.

--- plottillpressure.c
+++ plottillpressure.c
@@ -16,108 +16,196 @@
 int main(int argc, char **argv)
 {
     // Tillotson EOS library
-	TILLMATERIAL *tillmat;
-	double dKpcUnit = 2.06701e-13;
-	double dMsolUnit = 4.80438e-08;
-	double rho_max = 100.0;
-	double v_max = 100.0;
-	int nRho = 1000;
-	int nV = 1000;
+    TILLMATERIAL *tillmat;
+    double dKpcUnit = 2.06701e-13;
+    double dMsolUnit = 4.80438e-08;
+    double rho_max = 100.0;
+    double v_max = 100.0;
+    int nRho = 1000;
+    int nV = 1000;
     int nU = 1000;
+    int nT = 1000;
     int iMat = GRANITE;
-	double rho, u;
+    double rho, u, T;
     double rho_min = 1e-4;
     double u_min = 0.0;
     double u_max = 100.0;
+    double T_min = 0.0;
+    double T_max = 1e4;
     double drho;
     double du;
+    double dT;
     FILE *fp;
-	int i, j;
+    int i, j;
 
-	fprintf(stderr, "Initializing material...\n");
-	tillmat = tillInitMaterial(iMat, dKpcUnit, dMsolUnit);
-	fprintf(stderr, "Done.\n");
+    fprintf(stderr, "Initializing material...\n");
+    tillmat = tillInitMaterial(iMat, dKpcUnit, dMsolUnit);
+    fprintf(stderr, "Done.\n");
 
-	//tillmat = tillInitMaterial(iMat, dKpcUnit, dMsolUnit, nTableRho, nTableV, rhomax, vmax, 1);
+    tillInitLookup(tillmat, nRho, nV, rho_min, rho_max, v_max);
 
-	/*
-	 * Print P on a rho x u grid.
-	 */	
-	fp = fopen("plottillpressure.txt", "w");
-	assert(fp != NULL);
+    /*
+     * Zoom in to the expanded states.
+     */
+    rho_max = 1.1*tillmat->rho0;
+    u_max = 1.1*tillmat->us2;
 
-	fprintf(stderr, "Printing grid using tillPressure()...\n");
+    /*
+     * Print P on a rho x u grid.
+     */	
+    fp = fopen("plottillpressure.txt", "w");
+    assert(fp != NULL);
+
+    fprintf(stderr, "Printing grid using tillPressure()...\n");
 
     drho = (rho_max-rho_min)/(nRho-1);
     du = (u_max-u_min)/(nU-1);
 
-	/* Print a rho x u grid. */
-	for (i=0; i<nRho; i++)
-	{
-		rho = rho_min + i*drho;
+    for (j=0; j<nU; j++)
+    {
+        u = u_min + j*du;
 
-		for (j=0; j<nU; j++)
-		{
-			u = u_min + j*du;
-			fprintf(fp," %15.7E", tillPressure(tillmat, rho, u));
-		}
-		fprintf(fp,"\n");
-	}
-	fclose(fp);
+        for (i=0; i<nRho; i++)
+        {
+            rho = rho_min + i*drho;
+            fprintf(fp," %15.7E", tillPressure(tillmat, rho, u));
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
 
-#if !defined(TILL_PRESS_NP) || !defined(TILL_PRESS_MELOSH)
+    /* Print where the pressure is negative. */
+    fp = fopen("plottillpressure2.txt", "w");
+    assert(fp != NULL);
+
+    fprintf(stderr, "Printing where P = 0.0 ...\n");
+
+    for (j=0; j<nU; j++)
+    {
+        u = u_min + j*du;
+
+        for (i=0; i<nRho; i++)
+        {
+            rho = rho_min + i*drho;
+            if (tillPressure(tillmat, rho, u) <= 0.0) {
+                fprintf(fp, "%2i", 0);
+            } else {
+                fprintf(fp, "%2i", 1);
+            }
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+
     /*
-     * Print where the pressure is negative.
-     */
-    fp = fopen("till_granite_np.txt","w");
-	assert(fp != NULL);
+     * Print P on a rho x T grid.
+     */	
+    fp = fopen("plottillpressure-rhot.txt", "w");
+    assert(fp != NULL);
 
-	fprintf(stderr, "Printing where P = 0.0 ...\n");
+    fprintf(stderr, "Printing grid using tillPressure()...\n");
 
+    drho = (rho_max-rho_min)/(nRho-1);
+    dT = (T_max-T_min)/(nT-1);
+
+    for (j=0; j<nT; j++)
+    {
+        T = T_min + j*dT;
+
+        for (i=0; i<nRho; i++)
+        {
+            rho = rho_min + i*drho;
+            fprintf(fp," %15.7E", eosPressureRhoT(tillmat, rho, T));
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+
+    /* Print where the pressure is negative. */
+    fp = fopen("plottillpressure2-rhot.txt", "w");
+    assert(fp != NULL);
+
+    fprintf(stderr, "Printing where P = 0.0 ...\n");
+
+    for (j=0; j<nT; j++)
+    {
+        T = T_min + j*dT;
+
+        for (i=0; i<nRho; i++)
+        {
+            rho = rho_min + i*drho;
+            if (eosPressureRhoT(tillmat, rho, T) <= 0.0) {
+                fprintf(fp, "%2i", 0);
+            } else {
+                fprintf(fp, "%2i", 1);
+            }
+        }
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
+
+
+#if 0
     rho = 0.0; 
-	while (rho < tillmat->rho0)
-	{
+    while (rho < tillmat->rho0)
+    {
         u = 0.0;
         u_max = u;
         while (u < tillmat->us2)
         {
             if (tillPressure(tillmat, rho, u) < 0.0)
             {
-			    u_max = u;
+                u_max = u;
             }
             u += 0.01;
-		}
+        }
 
-		fprintf(fp,"%15.7E %15.7E\n", rho,u_max);
+        fprintf(fp,"%15.7E %15.7E\n", rho,u_max);
         rho += 0.01;
-	}
+    }
+#endif
 #if 0
     rho = 0.0; 
-	while (rho < tillmat->rho0)
-	{
-		fprintf(fp,"%15.7E", rho);
+    while (rho < tillmat->rho0)
+    {
+        fprintf(fp,"%15.7E", rho);
         u = 0.0;
         while (u < tillmat->us2)
         {
             if (tillPressure(tillmat, rho, u) < 0.0)
             {
-			    fprintf(fp," %15.7E", u);
+                fprintf(fp," %15.7E", u);
             }
             u += 0.01;
-		}
+        }
 
-		fprintf(fp,"\n");
+        fprintf(fp,"\n");
         rho += 0.01;
-	}
+    }
 #endif
+
+//    fprintf(stderr, "Printing, where the two functions differ...\n");
+    
+    printf("# rho            u\n");
+
+    fp = fopen("plottillpressure-axis.txt", "w");
+    assert(fp != NULL);
+    
+    fprintf(fp, "# rho            u              T\n");
+
+    /* Print a rho x u grid. */
+    for (i=0; i<nRho; i++)
+    {
+        rho = rho_min + i*drho;
+        u = u_min + i*du;
+        T = T_min + i*dT;
+
+        fprintf(fp, "%15.7E%15.7E%15.7E\n", rho, u, T);
+    }
+
     fclose(fp);
-#endif
 
-
-	fprintf(stderr, "Printing, where the two functions differ...\n");
-    printf("# rho          u\n");
-
-	tillFinalizeMaterial(tillmat);
+    tillFinalizeMaterial(tillmat);
 
     return 0;
 }
--- plottillpressure.py
+++ plottillpressure.py
@@ -8,19 +8,169 @@ from numpy import *
 from matplotlib.pyplot import *
 
 """
+Setup the plot.
+"""
+# Set a font
+rcParams['font.family'] = 'serif'
+rcParams['font.size'] = 10.0
+
+# Legend
+# mpl.rcParams['legend.handlelength']  = 2.9
+rcParams['legend.handlelength']  = 0.5
+rcParams['legend.frameon']       = False
+rcParams['legend.numpoints']     = 1
+rcParams['legend.scatterpoints'] = 1
+
+# Adjust axes line width
+rcParams['axes.linewidth']   = 0.5
+
+# Adjust ticks
+rcParams['xtick.major.size'] = 4
+rcParams['xtick.minor.size'] = 2
+rcParams['ytick.major.size'] = 4
+rcParams['ytick.minor.size'] = 2
+
+# Adjust Font Size
+rcParams['xtick.labelsize']  = 'xx-small'
+rcParams['ytick.labelsize']  = 'xx-small'
+rcParams['axes.labelsize']   = 'x-small'
+
+# Set Up Figure, Single Column MNRAS
+fig = gcf()
+ax = gca()
+fig, ax = subplots(1,1)
+fig.set_size_inches(8.27*0.39,8.27*(6./8.)*0.39)
+
+# Physical constants used for unit convertion (cgs)
+KBOLTZ = 1.38e-16		# bolzman constant in cgs
+MHYDR  = 1.67e-24		# mass of hydrogen atom in grams
+MSOLG  = 1.99e33		# solar mass in grams
+GCGS   = 6.67e-8		# G in cgs
+KPCCM  = 3.085678e21		# kiloparsec in centimeters
+
+# These two values define the unit system we use
+dKpcUnit  = 2.06701e-13	        # kiloparsec in code units
+dMsolUnit = 4.80438e-08	        # solar mass in code units
+
+# Mass of Earth
+MEarth = 5.98e27		# g
+MSun   = 1.989e33	        # g
+
+"""
+Convert kboltz/mhydrogen to system units, assuming that
+G == 1.
+"""
+# code energy per unit mass --> erg per g
+dErgPerGmUnit = GCGS*dMsolUnit*MSOLG/(dKpcUnit*KPCCM)
+# code density --> g per cc
+dGmPerCcUnit = (dMsolUnit*MSOLG)/pow(dKpcUnit*KPCCM,3.0)
+# code time --> seconds
+dSecUnit = sqrt(1/(dGmPerCcUnit*GCGS))
+
+# The units are not nescessary but can be derived from the quanities above
+Lunit = dKpcUnit*KPCCM
+Munit = dMsolUnit*MSOLG
+
+"""
 data1 = loadtxt('out.old.dat',skiprows=3)
 data2 = loadtxt('out.dat',skiprows=3)
 """
 
-# Tillotson material
+# Load values for rho and u
+data = loadtxt('plottillpressure-axis.txt')
+
+rho = data[:, 0]
+u   = data[:, 1]
+T   = data[:, 2]
+
+print "rho_min=", min(rho), "rho_max=", max(rho)
+print "u_min  =", min(u), "u_max  =", max(u)
+
+# Convert to cgs
+rho /= dGmPerCcUnit
+u   /= dErgPerGmUnit
+
+#data /= (dErgPerGmUnit*dGmPerCcUnit)
+#data /= (dGmPerCcUnit*dErgPerGmUnit)
+
+rho_min = min(rho)
+rho_max = max(rho)
+
+u_min   = min(u)
+u_max   = max(u)
+
+T_min   = min(T)
+T_max   = max(T)
+
+print "rho_min=", min(rho), "rho_max=", max(rho)
+print "u_min  =", min(u), "u_max  =", max(u)
+
+# Reference density (material dependent)
+rho0 = 2.7
+us   = 3.5e10
+us2  = 1.8e11
+
+# Plot P(rho, u)
 data = loadtxt('plottillpressure.txt')
 
 print where(fabs(data) < 1e-10)
 
-figure(1)
-imshow(data)
+#figure(1)
+imshow(data, origin='lower', extent=(rho_min, rho_max, u_min, u_max), aspect='auto')
+
+plot([rho0, rho0], [u_min, u_max], '--', color='red')
+plot([rho_min, rho_max], [us, us], '--', color='red')
+plot([rho_min, rho_max], [us2, us2], '--', color='red')
+
+xlabel("Density [code units]")
+ylabel("Int. energy [code units]")
 
 colorbar()
+
+savefig('plottillpressure.press.png', dpi=300, bbox_inches='tight')
+
+# Plot where P(rho, u) <= 0
+data = loadtxt('plottillpressure2.txt')
+
+#figure(2)
+
+fig.clear()
+
+imshow(data, origin='lower', extent=(rho_min, rho_max, u_min, u_max), aspect='auto')
+
+colorbar()
+
+savefig('plottillpressure.np.png', dpi=300, bbox_inches='tight')
+
+# Plot P(rho, T)
+data = loadtxt('plottillpressure-rhot.txt')
+
+print where(fabs(data) < 1e-10)
+
+#figure(1)
+imshow(data, origin='lower', extent=(rho_min, rho_max, T_min, T_max), aspect='auto')
+
+xlabel("Density [code units]")
+ylabel("Temperature [K]")
+
+colorbar()
+
+savefig('plottillpressure.press.rhot.png', dpi=300, bbox_inches='tight')
+
+# Plot where P(rho, u) <= 0
+data = loadtxt('plottillpressure2-rhot.txt')
+
+#figure(2)
+
+fig.clear()
+
+imshow(data, origin='lower', extent=(rho_min, rho_max, T_min, T_max), aspect='auto')
+
+colorbar()
+
+savefig('plottillpressure.np.rhot.png', dpi=300, bbox_inches='tight')
+
+
 
 show()
 
