efac453255bfa0b2bb7ecddd51756a5946783bb6
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2019-08-23 09:09:40 +0200

    Added a header file that contains the material ids from older versions to update old code or old ICs.

---
+++ tilloldmatid.h
@@ -0,0 +1,21 @@
+/*
+ * The header file for the Tillotson EOS library.
+ *
+ * Author:   Christian Reinhardt
+ * Date:     23.09.2019
+ * Modified: 
+ */
+#ifndef TILLOTSON_OLD_MAT_ID_HINCLUDED
+#define TILLOTSON_OLD_MAT_ID_HINCLUDED
+
+/*
+ * These are the old material ids for version 1.
+ */
+#define GRANITE_V1  0
+#define IRON_V1     1
+#define BASALT_V1   2
+#define ICE_V1      3
+#define WATER_V1    4
+
+#endif
+
