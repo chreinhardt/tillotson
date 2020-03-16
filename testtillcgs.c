be210130b880d23195151639771b6c56d7ee905c
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2020-03-16 21:08:57 +0100

    All quantities are calculated in cgs and give the correct resuls.

--- testtillcgs.c
+++ testtillcgs.c
@@ -10,24 +10,17 @@
 #include <assert.h>
 #include "tillotson.h"
 
-#define INDEX(i, j) ((i*tillMat->nTableV) + (j))
-
 int main(int argc, char **argv) {
     TILLMATERIAL *tillMat;
     /* Unit convertion factors */
-    double dKpcUnit = 2.06701e-13;
-    double dMsolUnit = 4.80438e-08;
     double rho, u;
     double P, cs;
-    //int nSteps;
     int iMat;
-    int i, j;
 
     iMat = 1;
 
     /* Initalize the material in cgs. */
-    tillMat = tillInitMaterial(iMat, dKpcUnit, dMsolUnit);
-    //tillMat = tillInitMaterial(iMat, 0.0, 0.0);
+    tillMat = tillInitMaterial(iMat, 0.0, 0.0);
 
     /* Print the material parameters. */
     tillPrintMat(tillMat);
